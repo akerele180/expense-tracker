aea65ccee597aae37e6c14b6c2dacaa03d07797b
Tosin Akerele <akerele.james1699@gmail.com>
2021-10-13 16:27:34 +0100

    making use of the local storage

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ const getMoney = document.getElementById("money-input");
 const getText = document.getElementById("text-input");
 const incomeTotal = document.getElementById("income");
 const expenseTotal = document.getElementById("expense");
-const incomeList = document.getElementById("item-green");
-const expenseList = document.getElementById("item-red");
 const balance = document.getElementById("balance");
 const list = document.getElementById("list");
 
-const dummyTransactions = [
-  { id: 1, text: "rice", amount: -500 },
-  { id: 2, text: "cash", amount: 600 },
-  { id: 2, text: "data", amount: -1000 },
-  { id: 2, text: "giveaway", amount: 3000 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: "rice", amount: -500 },
+//   { id: 2, text: "cash", amount: 600 },
+//   { id: 3, text: "data", amount: -1000 },
+//   { id: 4, text: "giveaway", amount: 3000 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
 // FUNTION DEFINITIONS
 // Add transaction to DOM list
 function addTransaction(e) {
@@ -26,7 +30,7 @@ function addTransaction(e) {
     const transaction = {
       id: generateID(),
       text: getText.value,
-      amount: +getMoney.value,
+      amount: +getMoney.value, //toconvert from string data type into number data type
     };
 
     transactions.push(transaction);
@@ -35,16 +39,13 @@ function addTransaction(e) {
 
     getText.value = "";
     getMoney.value = "";
-    console.log(transactions);
   }
 }
 
 function generateID() {
-  return Math.floor(Math.random() * 100000);
+  return Math.floor(Math.random() * 1000000);
 }
-// function removeTransaction(transactions.id) {
-//   return
-// }
+
 function addTransactionDom(transactions) {
   // setting up the sign convention
   const sign = transactions.amount > 0 ? "+" : "-";
@@ -66,27 +67,37 @@ function updateValues() {
   const amounts = transactions.map((transaction) => {
     return transaction.amount;
   });
-  console.log(amounts);
+  console.log(transactions);
 
-  const total = amounts.reduce((acc, item) => (acc += item)).toFixed(2);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   const income = amounts
     .filter((amount) => amount > 0)
-    .reduce((acc, item) => (acc += item))
+    .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
   const expense =
     amounts
       .filter((amount) => amount < 0)
-      .reduce((acc, item) => (acc += item))
-      .toFixed(2) * -1;
+      .reduce((acc, item) => (acc += item), 0) * -(1).toFixed(2);
 
+  console.log(expense);
   balance.innerText = `₦${total}`;
   incomeTotal.innerText = `₦${income}`;
   expenseTotal.innerText = `₦${expense}`;
 }
 
 updateValues();
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+  init();
+}
+
+// Update Local Storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
 
 // init app
 function init() {
